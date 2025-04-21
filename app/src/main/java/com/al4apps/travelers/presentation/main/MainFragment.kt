aacc6d8d2d9ed3f69b451e252903e9330d18b91d
Alisher Abdurashidov <abdurashidov.au@gmail.com>
2025-04-21 11:46:57 +0700

    [+] Added MainFragment and styles

---
+++ app/src/main/java/com/al4apps/travelers/presentation/main/MainFragment.kt
@@ -0,0 +1,8 @@
+package com.al4apps.travelers.presentation.main
+
+import com.al4apps.travelers.databinding.FragmentMainBinding
+import com.al4apps.travelers.presentation.utils.AbstractFragment
+
+class MainFragment: AbstractFragment<FragmentMainBinding>(FragmentMainBinding::inflate) {
+
+}
