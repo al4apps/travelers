9238dde05a8e405336190509a6b6f245d78e5d4c
Alisher Abdurashidov <abdurashidov.au@gmail.com>
2025-04-21 14:23:18 +0700

    [+] Added SearchFragment. Realized basic navigation with BottomNav

--- app/src/main/java/com/al4apps/travelers/presentation/main/MainFragment.kt
+++ app/src/main/java/com/al4apps/travelers/presentation/main/MainFragment.kt
@@ -1,8 +1,22 @@
 package com.al4apps.travelers.presentation.main
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.al4apps.travelers.databinding.FragmentMainBinding
 import com.al4apps.travelers.presentation.utils.AbstractFragment
 
 class MainFragment: AbstractFragment<FragmentMainBinding>(FragmentMainBinding::inflate) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        setupNavController()
+    }
+
+    private fun setupNavController() {
+        val navHost = binding.mainFragmentContainerView.getFragment<NavHostFragment>()
+        val navController = navHost.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
 }
---
+++ app/src/main/java/com/al4apps/travelers/presentation/search/SearchFragment.kt
@@ -0,0 +1,8 @@
+package com.al4apps.travelers.presentation.search
+
+import com.al4apps.travelers.databinding.FragmentSearchBinding
+import com.al4apps.travelers.presentation.utils.AbstractFragment
+
+class SearchFragment: AbstractFragment<FragmentSearchBinding>(FragmentSearchBinding::inflate) {
+
+}
