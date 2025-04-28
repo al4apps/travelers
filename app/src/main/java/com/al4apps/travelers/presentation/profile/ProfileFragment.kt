b4c07b56229402251e4f0834b47031556e98badb
Alisher Abdurashidov <abdurashidov.au@gmail.com>
2025-04-28 16:40:46 +0700

    [+] Создан экран Личный кабинет - ProfileFragment

---
+++ app/src/main/java/com/al4apps/travelers/presentation/profile/ProfileFragment.kt
@@ -0,0 +1,8 @@
+package com.al4apps.travelers.presentation.profile
+
+import com.al4apps.travelers.databinding.FragmentProfileBinding
+import com.al4apps.travelers.presentation.utils.AbstractFragment
+
+class ProfileFragment: AbstractFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
+
+}
