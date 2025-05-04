6b1cd65f0b58731d7671422867c0d7e85862e5d8
Alisher Abdurashidov <abdurashidov.au@gmail.com>
2025-05-04 19:33:10 +0700

    [+] Создан экран Настроек SettingsFragment.

---
+++ app/src/main/java/com/al4apps/travelers/presentation/settings/SettingsFragment.kt
@@ -0,0 +1,8 @@
+package com.al4apps.travelers.presentation.settings
+
+import com.al4apps.travelers.databinding.FragmentSettingsBinding
+import com.al4apps.travelers.presentation.utils.AbstractFragment
+
+class SettingsFragment : AbstractFragment<FragmentSettingsBinding>(FragmentSettingsBinding::inflate) {
+
+}
